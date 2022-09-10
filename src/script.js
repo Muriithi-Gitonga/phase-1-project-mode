9f36df0ed9f986e772b5730de939b8f204da0a06
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-09-10 15:11:34 +0300

    changed the html structure and css

--- src/script.js
+++ src/script.js
@@ -1,79 +1,187 @@
-// adding event listeners
-document.addEventListener('DOMContentLoaded', () => {
-//  adding event listener to the comment form
-    let comment = document.getElementById('commentform')
-    
-    comment.addEventListener('submit', (e) => {
-        e.preventDefault()
-        
-        addComment(e.target.feedback.value)
-        comment.reset()
-    })
+// https://api.disneyapi.dev/characters
 
+
+
+// renders a one beer on the dom
+function renderCharacter (data) {
+    let beerName = document.querySelector("#character-name");
+    beerName.innerText = data.name
+    let beerImage = document.querySelector("#character-image");
+    beerImage.setAttribute("src", data.imageUrl);
+    //add review from the server
+    let reviews = document.querySelector("#review-list")
+    let reviewList = reviews.querySelectorAll("li")
+    
+    reviewList[1].innerHTML = data.reviews
+
+    
+
+}
+
+// deleting review from the dom
+
+let reviewList = document.querySelector("#review-list")
+console.log(reviewList)
+
+
+
+
+
+
+async function initialize () {
+    let oneCharacter = await(await (fetch("https://api.disneyapi.dev/characters/6"))).json()
+    renderCharacter(oneCharacter)
+}
+
+
+initialize()
+
+
+
+
+
+// renders beer names to the navbar and an event listener
+function renderAllCharacterNames (character) {
+  
+    let ul = document.querySelector("ul")
+    character['data'].forEach(char => {
+        let li = document.createElement('li')
+        li.innerHTML = char.name
+        ul.appendChild(li)
+        li.addEventListener('click', () =>renderCharacter(char))
+    });    
+}
+
+
+
+
+async function initializeAll () {
+    let characterObj = await(await (fetch("https://api.disneyapi.dev/characters"))).json()
+    renderAllCharacterNames(characterObj) 
+}
+
+
+
+initializeAll()
+
+
+//add review from the customer
+
+document.querySelector('#review-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let customerReview = e.target.review.value
+    let reviews = document.querySelector("#review-list")
+    let reviewList = reviews.querySelectorAll("li")
+    
+    reviewList[0].innerHTML = customerReview
+    
+    
 })
 
 
-// renders value of the comment to the dorm
-function addComment (userComment) {
-    let comment = document.createElement('li')
-    comment.textContent = userComment
-    document.getElementById("comment").appendChild(comment)
-
-}
 
 
 
 
-//fetching the data from a public api
-async function get () {
-    let disneyObj = await (await fetch("https://api.disneyapi.dev/characters")).json()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // adding event listeners
+// document.addEventListener('DOMContentLoaded', () => {
+// //  adding event listener to the comment form
+//     let comment = document.getElementById('commentform')
     
-    disneyObj["data"].forEach(character => renderCharacter(character))
-}
-get()
+//     comment.addEventListener('submit', (e) => {
+//         e.preventDefault()
+        
+//         addComment(e.target.feedback.value)
+//         comment.reset()
+//     })
 
-//rendering the data from the public api to the dom
+// })
 
-function renderCharacter (character) {
-    let li = document.createElement('li')
-    li.className= "listcharacters"
-    li.innerHTML = `   
-    <img class="images" src = "${character['imageUrl']}">
-    <h3 class='name'> ${character['name']} </h3>
-    `
-    let form = document.createElement('form')
-    form.id = "users"
-    form.innerHTML = `
-    <label for="fname"><h3>Add a description</h3></label>
-    <input type="text" id="description"  placeholder="Description">
-    <input type="submit" value="Add description">
+
+// // renders value of the comment to the dorm
+// function addComment (userComment) {
+//     let comment = document.createElement('li')
+//     comment.textContent = userComment
+//     document.getElementById("comment").appendChild(comment)
+
+// }
+
+
+
+// // https://api.disneyapi.dev/characters
+// //fetching the data from a public api
+// async function get () {
+//     let disneyObj = await (await fetch("https://api.disneyapi.dev/characters")).json()
     
-    `
+//     disneyObj["data"].forEach(character => renderCharacter(character))
+// }
+// get()
 
-    let userDescription = document.createElement('div')
-    userDescription.id = "userDescription"
-    li.appendChild(form)
-    form.appendChild(userDescription)
+// //rendering the data from the public api to the dom
+
+// function renderCharacter (character) {
+//     let li = document.createElement('li')
+//     li.className= "listcharacters"
+//     li.innerHTML = `   
+//     <img class="images" src = "${character['imageUrl']}">
+//     <h3 class='name'> ${character['name']} </h3>
+//     `
+//     let form = document.createElement('form')
+//     form.id = "users"
+//     form.innerHTML = `
+//     <label for="fname"><h3>Add a description</h3></label>
+//     <input type="text" id="description"  placeholder="Description">
+//     <input type="submit" value="Add description">
     
-    document.getElementById("character-list").appendChild(li)
+//     `
+
+//     let userDescription = document.createElement('ul')
+//     userDescription.id = "userDescription"
+//     li.appendChild(form)
+//     form.appendChild(userDescription)
     
-    //adding event listener for  the description form 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault()
-        addDescription(e.target.description.value)
-    })
+//     document.getElementById("character-list").appendChild(li)
+    
+//     //adding event listener for  the description form 
+//     form.addEventListener('submit', (e) => {
+//         e.preventDefault()
+//         addDescription(e.target.description.value)
+//     })
     
     
 
-}
-// adding the description to the dom
-function addDescription (descriptionValue) {
-    let userDescription = document.createElement('li')
-    userDescription.textContent = descriptionValue
-    document.getElementById('userDescription').appendChild(userDescription)
+// }
+// // adding the description to the dom
+// function addDescription (descriptionValue) {
+//     let userDescription = document.createElement('li')
+//     userDescription.textContent = descriptionValue
+//     document.getElementById('userDescription').appendChild(userDescription)
 
 
-}
+// }
 
 
 
